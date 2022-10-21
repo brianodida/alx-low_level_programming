c5dede0ea1df904fa7c0628aea5c145fb28591e5
brianodida <odidabrian2@gmail.com>
2022-10-21 03:18:34 -0700

    this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stiod.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void convert_remaining_days(int month, int day);
+;
+int convert_day(int month, int day);
+
+#endif
